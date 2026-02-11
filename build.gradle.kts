8e0fbd0fdead5c48993cdaaa2d632ee0a41b66ee
Tradie <reece.liddelow@outlook.com>
2026-02-11 16:30:48 +0800

    Added custom hex values.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,27 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "com.tldchatcolor"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 tasks.test {
